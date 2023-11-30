41b0f4f29ae91af60c3a48f3e896fd6d85b86c6e
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2023-11-30 20:19:17 +0100

    add basic login and registration forms/logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
@@ -21,9 +24,10 @@ class AuthController extends Controller
 
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return redirect('welcome');
+            return redirect()->intended(route('welcome'));
+//            return view('welcome');
         }
-        return redirect(route('login'))->with("error", "login credentials not valid");
+        return redirect(route('register'))->with("error", "login credentials not valid");
 
     }
 
@@ -36,9 +40,32 @@ class AuthController extends Controller
     {
         $request->validate([
             'name' => ['required'],
-            'email' => ['required'],
+            'email' => ['required', 'unique:users', 'email'],
             'password' => ['required']
         ]);
-        return "registered";
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+        $data['role'] = "USER";
+
+        $user = User::create($data);
+
+        if (!$user) {
+            return redirect(route('register'))->with("error", "Registration failed");
+        }
+
+        return redirect(route('login'))->with("success", "Registration successful, please log in");
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        Auth::logout();
+        return redirect(route('login'))->with("success", "You have logged out successfully");
+    }
+
+    public function welcome()
+    {
+        return view('welcome');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/welcome', [AuthController::class, 'welcome'])->name('welcome');
 
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 
