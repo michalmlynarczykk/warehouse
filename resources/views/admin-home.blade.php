1af9c1f9cc28a2427d436864832e9f5376ecfd69
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2023-12-05 21:21:40 +0100

    rename home view

--- resources/views/admin-home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout')
-@section('title', 'Login')
-@section('content')
-<h1>Admin Home site</h1>
-@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('title', 'Home')
+@section('content')
+<h1>Home site</h1>
+@endsection
