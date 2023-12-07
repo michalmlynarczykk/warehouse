bbdac69d33c44c7caa898ff6f2528d3ae5fe468c
Michal Lakomy <mlakomy95@gmail.com>
2023-12-07 18:48:23 +0100

    edit views login, register panel and navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
 @section('title', 'Home')
 @section('content')
-<h1>Home site</h1>
+    <h1>Home site</h1>
 @endsection
--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,24 +1,24 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <p class="navbar-brand">Warehouse</p>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <a class="navbar-brand" href="{{ route('home') }}">Warehouse</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('home')}}">home</a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item {{ request()->routeIs('home') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('home') }}">Home</a>
             </li>
             @auth
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('logout')}}">logout</a>
+                    <a class="nav-link" href="{{ route('logout') }}">Logout</a>
                 </li>
             @else
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('register')}}">register</a>
+                <li class="nav-item {{ request()->routeIs('register') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('login')}}">login</a>
+                <li class="nav-item {{ request()->routeIs('login') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
                 </li>
             @endauth
         </ul>
