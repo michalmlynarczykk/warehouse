039ef8d2c57b46d9054542df902701d922e8c1ad
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2023-12-16 17:40:47 +0100

    add address fields to order form

--- resources/views/items/all.blade.php
+++ resources/views/items/all.blade.php
@@ -16,7 +16,6 @@
                     <th>Price</th>
                     <th>Available Amount</th>
                     <th>Order Quantity</th>
-                    <th>Order</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -30,10 +29,6 @@
                             <input type="number" name="order_items[{{ $item->id }}][quantity]" value="0" min="0"
                                    max="{{ $item->available_amount }}">
                         </td>
-                        <td>
-                            <input type="checkbox" name="order_items[{{ $item->id }}][checked]"
-                                   value="{{ $item->id }}">
-                        </td>
                     </tr>
                 @empty
                     <tr>
@@ -42,14 +37,31 @@
                 @endforelse
                 </tbody>
             </table>
-            <div class="d-flex justify-content-center">
-                {{ $items->links('pagination::simple-bootstrap-4') }}
+
+            <!-- Add fields for Address model -->
+            <div class="mb-3">
+                <label for="street" class="form-label">Street:</label>
+                <input type="text" class="form-control" id="street" name="street" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="city" class="form-label">City:</label>
+                <input type="text" class="form-control" id="city" name="city" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="state" class="form-label">State:</label>
+                <input type="text" class="form-control" id="state" name="state" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="zip_code" class="form-label">Zip Code:</label>
+                <input type="text" class="form-control" id="zip_code" name="zip_code" required>
             </div>
 
             <div class="d-flex justify-content-center mt-1">
                 <button type="submit" class="btn btn-success">Place Order</button>
             </div>
-
         </form>
     </div>
 @endsection
