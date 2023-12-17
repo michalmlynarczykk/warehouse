1aec9ed459ea050cfd726be7072a167bdb31ae8f
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2023-12-17 14:03:45 +0100

    add sorting and filter for items

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -26,15 +26,31 @@ class ItemController extends Controller
     }
 
 
-    public function all()
+    public function all(Request $request)
     {
-        $items = Item::all();
+        $items = $this->getItems($request);
         return view('items.all', compact('items'));
     }
 
-    public function adminAll()
+    public function adminAll(Request $request)
     {
-        $items = Item::paginate(10);
+        $items = $this->getItems($request);
         return view('items.admin_all', compact('items'));
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getItems(Request $request)
+    {
+        $filter = $request->query('filter');
+        if (!empty($filter)) {
+            return Item::sortable()
+                ->where('name', 'like', '%' . $filter . '%')
+                ->paginate(10);
+        } else {
+            return Item::sortable()
+                ->paginate(10);
+        }
     }
 }
--- resources/views/items/admin_all.blade.php
+++ resources/views/items/admin_all.blade.php
@@ -1,42 +1,51 @@
 <!-- resources/views/items/all.blade.php -->
-
 @extends('layout')
-
 @section('content')
     <div class="container mt-5">
         <h2>Items</h2>
 
-        <table class="table">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Available Amount</th>
-            </tr>
-            </thead>
-            <tbody>
-            @forelse($items as $item)
-                <tr>
-                    <td>{{ $item->id }}</td>
-                    <td>{{ $item->name }}</td>
-                    <td>{{ $item->price }}</td>
-                    <td>{{ $item->available_amount }}</td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="5">No items found.</td>
-                </tr>
-            @endforelse
-            </tbody>
-        </table>
+        <form action="{{ route('items.admin_all') }}" class="form-inline" method="GET">
+            <div class="form-group mb-2">
+                <label for="filter" class="col-sm-2 col-form-label">Filter</label>
+                <input type="text" class="form-control" id="filter" name="filter" placeholder="Item name..."
+                       value="{{ $filter ?? '' }}">
+            </div>
+            <button type="submit" class="btn btn-secondary mb-2">Filter</button>
+        </form>
+        <div class="container mt-5">
+            <h2>Items</h2>
 
-        <div class="d-flex justify-content-center">
-            {{ $items->links('pagination::simple-bootstrap-4') }}
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>@sortablelink('name', 'Name')</th>
+                    <th>@sortablelink('price', 'Price')</th>
+                    <th>@sortablelink('available_amount', 'Available Amount')</th>
+                </tr>
+                </thead>
+                <tbody>
+                @forelse($items as $item)
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->price }}</td>
+                        <td>{{ $item->available_amount }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="5">No items found.</td>
+                    </tr>
+                @endforelse
+                </tbody>
+            </table>
+
+            <div class="d-flex justify-content-center">
+                {{ $items->links('pagination::simple-bootstrap-4') }}
+            </div>
+
+            <a class="d-flex justify-content-center mt-1" href="{{ route('items.create') }}">
+                <button type="submit" class="btn btn-success">Add new item</button>
+            </a>
         </div>
-
-        <a class="d-flex justify-content-center mt-1" href="{{ route('items.create') }}">
-            <button type="submit" class="btn btn-success">Add new item</button>
-        </a>
-    </div>
 @endsection
